b7c837ddc38c503b287bdf7bc547436b988cda57
Chris Churas <churas.camera@gmail.com>
2023-08-14 14:14:17 -0700

    updated team name

--- cellmaps_image_embedding/__init__.py
+++ cellmaps_image_embedding/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for cellmaps_image_embedding."""
 
-__author__ = 'Ideker Lab CM4AI team'
+__author__ = 'Ideker Cell Maps team'
 __email__ = 'tools@cm4ai.org'
 __version__ = '0.1.0a9'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_image_embedding'
