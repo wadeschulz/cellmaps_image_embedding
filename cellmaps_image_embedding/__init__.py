e4d2ad1f6756bc15edd852261a5e1b4df648728d
Chris Churas <churas.camera@gmail.com>
2024-02-01 11:23:04 -0800

    set release to today. removed alpha from version

--- cellmaps_image_embedding/__init__.py
+++ cellmaps_image_embedding/__init__.py
@@ -2,9 +2,9 @@
 
 """Top-level package for cellmaps_image_embedding."""
 
-__author__ = 'Ideker Cell Maps team'
+__author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a14'
+__version__ = '0.1.0'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_image_embedding'
 __description__ = 'A tool to generate embeddings from HPA IF images'
 __computation_name__ = 'IF Image Embedding'
